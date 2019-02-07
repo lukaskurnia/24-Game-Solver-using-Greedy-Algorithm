4a9bb1eab2ec24d974334dd260de08ad5ee3e982
Christopher <christopher.billys@gmail.com>
2019-02-07 23:48:16 +0700

    FrontEnd updated

--- Final/code/frontend.py
+++ Final/code/frontend.py
@@ -4,6 +4,10 @@ import random
 from kivy.app import App
 from kivy.lang import Builder
 
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.text import LabelBase
+from kivy.uix.textinput import TextInput
+
 from kivy.uix.boxlayout import BoxLayout
 
 #from kivy.uix.screenmanager import Screen, ScreenManager
@@ -17,7 +21,69 @@ from kivy.graphics import Color, Rectangle
 # Tampilan frontend
 Builder.load_string('''
 #:import hex kivy.utils.get_color_from_hex
-<setScreen>:
+<Login>:
+    BoxLayout:
+        orientation: 'vertical'
+        padding : 30, 30
+        canvas.before:
+            Color:
+                rgba: hex('006400')
+            Rectangle:
+                size: self.size
+                pos: self.pos
+        Label:
+            text: "Fake 24 Solver"
+            font_size: 70
+            bold: True
+            italic: True
+            pos_hint: {'center_x': 0.5, 'center_y': 0.65}
+        BoxLayout:
+            orientation: "horizontal"
+            padding: 10, 10
+            spacing: 10, 10
+            size_hint_x: .7
+            canvas.before:
+                Color:
+                    rgba: hex('006400')
+                Rectangle:
+                    size: self.size
+                    pos: self.pos
+            Label:
+                text: "Username   "
+                text_size: self.size
+                halign: 'right'
+                size_hint_x: .5
+                size_hint_y: .3
+                pos_hint: {'center_x': 0.7, 'center_y': 0.79}
+
+            TextInput:
+                multiline: False
+                size_hint_x: .5
+                size_hint_y: .15
+                pos_hint: {'center_x': 0.4, 'center_y': 0.65}
+                id : tester
+        Button:
+            text: 'LOGIN'
+            padding: 200, 200
+            spacing: 10, 10
+            valign: 'middle'
+            size_hint_x: .2
+            size_hint_y: .2
+            pos_hint: {'center_x': 0.5, 'center_y': 0.8}
+            on_press:
+                root.manager.transition.direction = 'left'
+                root.manager.current = 'Play'
+                root.login()
+        Button:
+            text: 'Quit'
+            padding: 100, 100
+            spacing: 10, 10
+            size_hint_x: .2
+            size_hint_y: .2
+            pos_hint: {'center_x': 0.5, 'center_y': 0.8}
+            on_press: app.stop()
+
+<Play>:
     BoxLayout:
         orientation: 'vertical'
         spacing: 10
@@ -31,12 +97,17 @@ Builder.load_string('''
             orientation: 'horizontal'
             padding: 10
             spacing: 10
-            Label:
-                text: "Fake 24 Solver"
-                font_size: 50
-                bold: True
-                italic: True
-                pos_hint: {'center_x': -1, 'center_y': 0.65}
+            BoxLayout:
+                orientation: 'vertical'
+                Label:
+                    id: title
+                    text: ""
+                    font_size: 50
+                    bold: True
+                    italic: True
+                Label:
+                    id: cards
+                    text: "Cards remaining: 52"
             BoxLayout:
                 orientation: 'horizontal'
                 padding: 5
@@ -51,12 +122,15 @@ Builder.load_string('''
                     spacing: 20
                     Button:
                         text: "DRAW and SOLVE!"
+                        bold: True
                         on_press: root.DaS()
                     Button:
                         text: "Return to deck"
+                        bold: True
                         on_press: root.rebound()
                     Button:
                         text: "Exit"
+                        bold: True
                         on_press: app.stop()
         BoxLayout:
             orientation: 'horizontal'
@@ -87,25 +161,50 @@ Builder.load_string('''
             BoxLayout:
                 orientation: 'vertical'
                 Label:
-                    text: "Solution:"
-                    font_size: 20
+                    text: "Solution :"
+                    font_size: 35
+                    bold: True
                 Label:
                     text: ""
                     id: solution
             BoxLayout:
-                orientation: 'vertical'
-                Label:
-                    text: "Score:"
-                    font_size: 20
-                Label:
-                    text: ""
-                    id: score
+                orientation: 'horizontal'
+                BoxLayout:
+                    orientation: 'vertical'
+                    Label:
+                        text: "Result :"
+                        font_size: 35
+                        bold: True
+                    Label:
+                        text: ""
+                        id: result
+                BoxLayout:
+                    orientation: 'vertical'
+                    Label:
+                        text: "Score :"
+                        font_size: 35
+                        bold: True
+                    Label:
+                        text: ""
+                        id: score
 ''')
 
 # List deck kartu
 deck = [True] * 52
 
-class setScreen(BoxLayout):
+# Nama pemain
+name = [""] * 2
+
+#class Screen1(BoxLayout):
+class Login(Screen):
+    def login(self):
+        name[0] = self.ids.tester.text
+
+
+class Play(Screen):
+    def on_enter(self):
+        self.ids.title.text = name[0]
+        print(self.ids.title.text)
     def DaS(self):
         if countDeck() == 0:
             self.ids.solution.text = "Fail, no solution"
@@ -132,7 +231,9 @@ class setScreen(BoxLayout):
             solution = backend.algorithm24(cards)
             score = backend.score24(solution)
             self.ids.solution.text = solution
+            self.ids.result.text = str(eval(solution))
             self.ids.score.text = str(score)
+            self.ids.cards.text = "Cards remaining = " + str(len(deck))
 
     def rebound(self):
         for i in range(len(deck)):
@@ -204,11 +305,13 @@ def mapping(num, sign):
     target += ".png"
     return target
 
-
+sm = ScreenManager()
+sm.add_widget(Login(name='Login'))
+sm.add_widget(Play(name='Play'))
 # Main Program
 class Main(App):
     def build(self):
-        return setScreen()
+        return sm #Play()
 
 if __name__ == '__main__':
     Main().run()
